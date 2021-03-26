0b56c7f8258d9a9173e432bdbfc1e37ffd70faf6
Charlie <charliedeveloperaccess@gmail.com>
2021-03-26 09:35:19 -0500

    temporarily shut down server when redis cannot connect

--- services/redis.js
+++ services/redis.js
@@ -1,8 +1,16 @@
 const Redis = require('ioredis');
 const redisJSON = require('redis-json');
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const redis = new Redis(REDIS_PORT);
-const redisJson = new redisJSON(redis);
+let redisJson;
+try {
+    const REDIS_PORT = process.env.REDIS_PORT || 6379;
+    const redis = new Redis(REDIS_PORT);
+    redisJson = new redisJSON(redis);
+    redisJson.set('init', true);
+    redisJSON.get('init');
+} catch(error) {
+    console.log(error, 'tried to connect to redis failed');
+    process.exit(0);
+}
 
 module.exports = redisJson;
